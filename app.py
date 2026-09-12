ac4447af649a69da5e37c7eddcd2dc0e369e2d5c
jeffreymayolo <jeffmayolo@g.harvard.edu>
2026-09-12 08:36:06 -0400

    saving progress

--- app.py
+++ app.py
@@ -213,5 +213,5 @@ def admin():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5001))
+    port = int(os.environ.get("PORT", 8000))
     app.run(host="0.0.0.0", port=port, debug=os.environ.get("FLASK_DEBUG") == "1")
